5374c13d7d50dd5da42a6eef20c7f4dbb0e599ca
Brooke Kullberg <brookekullberg@gmail.com>
2019-05-10 12:59:21 -0700

    fail and pass test for client constructor

--- HairSalon.Tests/ModelTests/ClientTests.cs
+++ HairSalon.Tests/ModelTests/ClientTests.cs
@@ -5,10 +5,19 @@ using HairSalon.Models;
 
 namespace HairSalon.Tests
 {
-  [TestClass]
-  public class ClientTests
-  {
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        public void ClientConstructor_CreatesInstanceOfClient_Client()
+        {
+            //Arrange
+            DateTime apt =  new DateTime(2019, 05, 19);
+            Client newClient = new Client("Bart", "hair cut", apt, 1);
 
+            //Assert
+            Assert.AreEqual(typeof(Client), newClient.GetType());
+        }
 
-  }
+    }
 }
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,11 +4,24 @@ using System;
 
 namespace HairSalon.Models
 {
-
     public class Client
     {
+        private int _id;
+        private string _name;
+        private string _serviceRequest;
+        private DateTime _appointment;
+        private int _stylistId;
 
+        public Client(string name, string serviceRequest, DateTime appointment, int stylistId, int id = 0)
+        {
+            _name = name;
+            _serviceRequest = serviceRequest;
+            _appointment = appointment;
+            _stylistId = stylistId;
+            _id = id;
+        }
 
+        
 
 
     }
