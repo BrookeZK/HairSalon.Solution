2388d6313242ab896dd9196a5343650e2eb6b579
Brooke Kullberg <brookekullberg@gmail.com>
2019-05-17 12:11:46 -0700

    fail/pass: add tests for get/set name and get id for specialty

--- HairSalon.Tests/ModelTests/SpecialtyTests.cs
+++ HairSalon.Tests/ModelTests/SpecialtyTests.cs
@@ -28,5 +28,48 @@ namespace HairSalon.Tests
             Assert.AreEqual(typeof(Specialty), newSpecialty.GetType());
         }
 
+        [TestMethod]
+        public void GetName_ReturnsName_String()
+        {
+            //Arrange
+            string name = "hair";
+            Specialty newSpecialty = new Specialty(name);
+
+            //Act
+            string result = newSpecialty.Name;
+
+            //Assert
+            Assert.AreEqual(name, result);
+        }
+
+        [TestMethod]
+        public void SetName_UpdateName_String()
+        {
+            //Arrange
+            string name1 = "hair";
+            string name2 = "color";
+            Specialty newSpecialty = new Specialty(name1);
+
+            //Act
+            string result = newSpecialty.Name = name2;
+
+            //Assert
+            Assert.AreEqual(name2, result);
+        }
+
+        [TestMethod]
+        public void GetId_ReturnsId_Int()
+        {
+            //Arrange
+            int id = 2;
+            Specialty newSpecialty = new Specialty("hair", id);
+
+            //Act
+            int result = newSpecialty.Id;
+
+            //Assert
+            Assert.AreEqual(id, result);
+        }
+
     }
 }
--- HairSalon/Models/Specialty.cs
+++ HairSalon/Models/Specialty.cs
@@ -32,6 +32,8 @@ namespace HairSalon.Models
             }
         }
 
+        
+
     }
 
 }
