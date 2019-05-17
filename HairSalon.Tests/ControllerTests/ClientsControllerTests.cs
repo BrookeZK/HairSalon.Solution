ae348c8075d8769982c5e1bde3cd3049e32ee218
Brooke Kullberg <brookekullberg@gmail.com>
2019-05-17 11:26:55 -0700

    add clientsControllertests for HttpGets

--- HairSalon.Tests/ControllerTests/ClientsControllerTests.cs
+++ HairSalon.Tests/ControllerTests/ClientsControllerTests.cs
@@ -1,26 +1,52 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using Microsoft.AspNetCore.Mvc;
-// using System.Collections.Generic;
-// using HairSalon.Controllers;
-// using HairSalon.Models;
-//
-// namespace HairSalon.Tests
-// {
-//     [TestClass]
-//     public class ClientsControllerTest
-//     {
-//         [TestMethod]
-//         public void New_ReturnsCorrectView_True()
-//         {
-//             //Arrange
-//             ClientsController controller = new ClientsController();
-//
-//             //Act
-//             ActionResult newView = controller.New();
-//
-//             //Assert
-//             Assert.IsInstanceOfType(newView, typeof(ViewResult));
-//         }
-//
-//     }
-// }
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using HairSalon.Controllers;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+    [TestClass]
+    public class ClientsControllerTest
+    {
+        [TestMethod]
+        public void New_ReturnsCorrectView_True()
+        {
+            //Arrange
+            ClientsController controller = new ClientsController();
+
+            //Act
+            ActionResult newView = controller.New(1);
+
+            //Assert
+            Assert.IsInstanceOfType(newView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Show_ReturnsCorrectView_True()
+        {
+            //Arrange
+            ClientsController controller = new ClientsController();
+
+            //Act
+            ActionResult newView = controller.Show(1, 3);
+
+            //Assert
+            Assert.IsInstanceOfType(newView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Edit_ReturnsCorrectView_True()
+        {
+            //Arrange
+            ClientsController controller = new ClientsController();
+
+            //Act
+            ActionResult newView = controller.Edit(2, 4);
+
+            //Assert
+            Assert.IsInstanceOfType(newView, typeof(ViewResult));
+        }
+
+    }
+}
